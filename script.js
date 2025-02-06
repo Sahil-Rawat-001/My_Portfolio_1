a8c4ffc1a7915f9e5d3d219e107b78e287ac6cd6
Sahil-Rawat-001 <rawatsahilwork001@gmail.com>
2025-02-06 21:42:04 +0530

    add contact and finalize project

--- script.js
+++ script.js
@@ -24,4 +24,23 @@ window.openTab = function(event,tablinkname){
     }
 }
 
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxhBL6_WUB5QxgI_adu3Nsq37e3pO7Oi7G1gfu6q4xLEJbQa3dmCrDeTtKGB_G_ooo/exec'
+const form = document.forms['submit-to-google-sheet']
+
+const msg = document.getElementById("msg");
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response =>{
+        msg.innerHTML = "Message sent successfully"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },5000);
+
+        form.reset();
+    })
+    .catch(error => console.error('Error!', error.message))
+})
+
 
